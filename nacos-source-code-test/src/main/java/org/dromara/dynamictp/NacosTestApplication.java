dd99ccc87a6eb038b3480e067187fe36d6265b4c
wuxin <myeagleandsnake@gaml.com>
2025-02-07 22:57:45 +0800

    feat: configLocader

--- nacos-source-code-test/src/main/java/org/dromara/dynamictp/NacosTestApplication.java
+++ nacos-source-code-test/src/main/java/org/dromara/dynamictp/NacosTestApplication.java
@@ -2,12 +2,14 @@ package org.dromara.dynamictp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wuxin
  * @date 2025/02/07 21:54:42
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NacosTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosTestApplication.class, args);
