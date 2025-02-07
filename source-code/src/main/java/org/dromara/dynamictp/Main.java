6dab91d4f0aaffcc8438d86f2edc84fb3f6e4510
wuxin <myeagleandsnake@gaml.com>
2025-02-07 21:48:25 +0800

    feat: source code read

---
+++ source-code/src/main/java/org/dromara/dynamictp/Main.java
@@ -0,0 +1,11 @@
+package org.dromara.dynamictp;
+
+/**
+ * @author wuxin
+ * @date 2025/02/07 21:47:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
