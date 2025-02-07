541447d57facb10a053fc3605ce328b18ec73238
wuxin <myeagleandsnake@gaml.com>
2025-02-07 21:57:30 +0800

    feat: source code read

---
+++ nacos-source-code-test/src/main/java/org/dromara/dynamictp/NacosTestApplication.java
@@ -0,0 +1,15 @@
+package org.dromara.dynamictp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuxin
+ * @date 2025/02/07 21:54:42
+ */
+@SpringBootApplication
+public class NacosTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosTestApplication.class, args);
+    }
+}
