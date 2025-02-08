deeeb2c5783c6aa4c676f2b92d15f19a8107d9ad
WUXIN <myeagleandsnake@gmail.com>
2025-02-08 11:19:18 +0800

    feat: change jdkl 的版本 cglib在17下运行异常

--- nacos-source-code-test/src/main/java/org/dromara/dynamictp/NacosTestApplication.java
+++ nacos-source-code-test/src/main/java/org/dromara/dynamictp/NacosTestApplication.java
@@ -1,8 +1,11 @@
 package org.dromara.dynamictp;
 
+import org.dromara.dynamictp.spring.annotation.EnableDynamicTp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.SpringVersion;
 
 /**
  * @author wuxin
@@ -10,8 +13,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableDynamicTp
+@EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
 public class NacosTestApplication {
     public static void main(String[] args) {
+        String version = SpringVersion.getVersion();
+        System.err.println(version);
         SpringApplication.run(NacosTestApplication.class, args);
     }
 }
